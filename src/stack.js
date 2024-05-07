bcfeaecc1e3c5508ee348a926178301ebc4723fc
Andy Mehmed <119579140+AndyMehmed@users.noreply.github.com>
2024-05-07 15:32:43 +0200

    Kopplat samman HTMl och JS

--- src/stack.js
+++ src/stack.js
@@ -16,3 +16,29 @@ exports.pop = function () {
 exports.peek = function () {
     return stack[0]; // Det här är medvetet felaktigt
 }
+
+import * as stack from './stack.js';
+
+window.onload = function () {
+    console.log("done");
+
+    var pop = document.getElementById('pop');
+    var push = document.getElementById('push');
+    var peek = document.getElementById('peek');
+    var display = document.getElementById('top_of_stack');
+
+    pop.addEventListener("click", function() {
+        var text = "Tog bort " + stack.pop();
+        alert(text);
+    });
+
+    push.addEventListener("click", function() {
+        var x = prompt("Vad ska vi lägga på stacken?");
+        stack.push(x);
+        display.innerHTML = x;
+    });
+
+    peek.addEventListener("click", function() {
+        display.innerHTML = stack.peek();
+    });
+};
